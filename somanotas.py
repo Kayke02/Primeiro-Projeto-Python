225f555ff81f69d966ff6800e7a983a664132763
KAYKE OLIVEIRA CORREA <202601399004@alunos.faculdadeideal.edu.br>
2026-08-18 20:31:16 -0300

    atualizando

--- somanotas.py
+++ somanotas.py
@@ -4,5 +4,14 @@ nota2=float(input("Digite a segunda nota: "))
 nota3=float(input("Digite a terceira nota: "))
 nota4=float(input("Digite a quarta nota: "))
 media=(nota1+nota2+nota3+nota4)/4
-
-print(f"A média das 4 notas ({nota1: .1f},{nota2: .1f},{nota3: .1f},{nota4: .1f} ) do aluno {nome} é {media: .2f}")
+if media >=7:
+    print(f"A média das 4 notas ({nota1: .1f},{nota2: .1f},{nota3: .1f},{nota4: .1f} ) do aluno {nome} é {media: .2f} e ele está aprovado")
+elif media<=4:
+    print(f"A média das 4 notas ({nota1: .1f},{nota2: .1f},{nota3: .1f},{nota4: .1f} ) do aluno {nome} é {media: .2f} e ele está reproprovado")
+else:
+    print(f"A média das 4 notas ({nota1: .1f},{nota2: .1f},{nota3: .1f},{nota4: .1f} ) do aluno {nome} é {media: .2f} e ele está de recuperação")
+notaR=float(input("Digite a nota da recuperação"))
+if notaR>=7:
+    print("A nota da recuperação do aluno",nome," foi", notaR, " e ele está aprovado")
+else:
+    print(f"A nota da recuperação do aluno {nome} foi {notaR} e ele está reprovado")
